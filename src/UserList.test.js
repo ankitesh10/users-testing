3103e5c1514037f30f823f172d198ab1cf87aac2
ankitesh <ankitesh4@gmail.com>
2023-04-26 21:04:40 +0530

    refactor done

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,34 +2,38 @@ import { render, screen, within } from "@testing-library/react";
 
 import UserList from "./UserList";
 
-test("render one row per user", () => {
-  // render the component
+function renderComponent() {
   const users = [
     { name: "jane", email: "jane@jame.com" },
     { name: "sam", email: "sam@sam.com" },
   ];
 
-  // render(<UserList users={users} />);
-  const { container } = render(<UserList users={users} />);
+  render(<UserList users={users} />);
+
+  return { users };
+}
+
+test("render one row per user", () => {
+  // render the component
+
+  renderComponent();
+
+  // const { container } = render(<UserList users={users} />);
 
   // Find all the rows in the table
 
   // screen.logTestingPlaygroundURL();
-  // const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  const rows = within(screen.getByTestId("users")).getAllByRole("row");
 
   // eslint-disable-next-line
-  const rows = container.querySelectorAll("tbody tr");
+  // const rows = container.querySelectorAll("tbody tr");
 
   // Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
 
 test("render the email and name of each user", () => {
-  const users = [
-    { name: "jane", email: "jane@jame.com" },
-    { name: "sam", email: "sam@sam.com" },
-  ];
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   users.forEach((user) => {
     const name = screen.getByRole("cell", { name: user.name });
